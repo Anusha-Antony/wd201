ab4b618070d844bd47d329aa4e13ebf665c17ee7
Anusha-Antony <anushaanuz2004@gmail.com>
2025-06-12 19:06:25 +0530

    Update index.js

--- todo-cli/models/index.js
+++ todo-cli/models/index.js
@@ -1,16 +1,51 @@
-const { Sequelize } = require('sequelize');
+/* eslint-disable no-undef */
+"use strict";
 
-// Create a Sequelize instance connected to your DB (adjust credentials)
-const sequelize = new Sequelize('database_name', 'username', 'password', {
-  host: 'localhost',
-  dialect: 'postgres', // or 'mysql', 'sqlite', etc.
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const process = require("process");
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.json")[env];
+const db = {};
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config,
+  );
+}
+
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 &&
+      file !== basename &&
+      file.slice(-3) === ".js" &&
+      file.indexOf(".test.js") === -1
+    );
+  })
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes,
+    );
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
 });
 
-const db = {};
-db.Sequelize = Sequelize;
 db.sequelize = sequelize;
-
-// Import models
-db.Todo = require('./todo')(sequelize, Sequelize);
+db.Sequelize = Sequelize;
 
 module.exports = db;
